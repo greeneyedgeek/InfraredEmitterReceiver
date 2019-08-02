fc061bd69bec8b37679b600654b4cb6f19758045
greeneyedgeek <35167787+greeneyedgeek@users.noreply.github.com>
2019-08-02 00:47:53 -0400

    Update file

---
+++ IR_Emitter/IR_Emitter.ino
@@ -0,0 +1,54 @@
+/*
+  Project:  InfraredEmitterReceiver
+  File:   	IR_Emitter.cpp
+  Created:  2019-07-27
+  Modified: 2019-08-02
+  Author:   Gabriel Fontaine-Escobar
+
+  This code is in the public domain.
+  https://github.com/greeneyedgeek/InfraredEmitterReceiver
+  
+  The circuit:
+  * IR LED to digital pin 3
+  * IR LED POS to +5V
+  * IR LED GND PIN to GROUND
+*/
+
+#include <Arduino.h>
+#include <IRremote.h>
+
+#define IR_LED 3
+
+class Signal
+{
+public:
+  // IR Signal constants
+  static const int OFF = 0x00;
+  static const int ON = 0x01;
+  static const int RANDOM = 0x02;  
+  static const int RED = 0x11;
+  static const int YELLOW = 0x55;
+  static const int GREEN = 0x0F;
+  static const int CYAN = 0xF0;
+  static const int BLUE = 0xFF;
+  static const int MAGENTA = 0x1F;
+  static const int WHITE = 0x99;
+
+  int VALUES[7] { RED, YELLOW, GREEN, CYAN, BLUE, MAGENTA, WHITE }; 
+};
+
+IRsend emitter;
+Signal signal;
+
+void setup()
+{
+	// Set IR LED Pin as output
+	pinMode(IR_LED, OUTPUT); 
+}
+
+void loop() 
+{
+  // Send a random colour signal every 250ms
+  emitter.sendNEC(signal.VALUES[random(7)], 32);
+  _delay_ms(250);  
+}
--- IR_Receiver/IR_Receiver.ino
+++ IR_Receiver/IR_Receiver.ino
@@ -1,20 +1,45 @@
+/*
+  Project:  InfraredEmitterReceiver
+  File:   	IR_Receiver.cpp
+  Created:  2019-07-27
+  Modified: 2019-08-02
+  Author:   Gabriel Fontaine-Escobar
+
+  This code is in the public domain.
+  https://github.com/greeneyedgeek/InfraredEmitterReceiver
+  
+  The circuit:
+  * IR RECEIVER to digital pin 2
+  * IR RECEIVER POS to +5V
+  * IR RECEIVER GND PIN to GROUND
+  * RGB LED GREEN PIN to digital pin 8
+  * RGB LED BLUE PIN to digital pin 9
+  * RGB LED RED PIN to digital pin 10
+  * RGB LED GND pin to GROUND
+*/
+
 #include <Arduino.h>
 #include <IRremote.h>
 
-#define IR_RECEIVER 2 //PD2
-#define RED_LED 10 //PB2
-#define BLUE_LED 9 //PB1
-#define GREEN_LED 8 //PB0
+#define IR_RECEIVER 2 // PD2
+#define RED_LED 10 // PB2
+#define BLUE_LED 9 // PB1
+#define GREEN_LED 8 // PB0
 
-// IR Signal constants
 class Signal
 {
 public:
+  // IR Signal constants
   static const int OFF = 0x00;
   static const int ON = 0x01;
+  static const int RANDOM = 0x02;  
   static const int RED = 0x11;
+  static const int YELLOW = 0x55;
   static const int GREEN = 0x0F;
+  static const int CYAN = 0xF0;
   static const int BLUE = 0xFF;
+  static const int MAGENTA = 0x1F;
+  static const int WHITE = 0x99;
 };
 
 // LED status indicator
@@ -26,98 +51,65 @@ Signal signal;
 IRrecv receiver(IR_RECEIVER);
 decode_results results;
 
+// Sets the LED colour
 void setRgb(int redValue, int greenValue, int blueValue) 
 {
     
-	analogWrite(RED_LED, redValue);
-	analogWrite(GREEN_LED, greenValue);
-	analogWrite(BLUE_LED, blueValue);
-	/*
-	if (redValue != 0) 
-    { 
-		// RED LED ON
-		PORTB |= (1 << RED_LED);
-    }
-    else 
-    {
-		// RED LED OFF
-		PORTB &= ~(1 << RED_LED);
-    }
-    if (greenValue != 0) 
-    { 
-		// GREEN LED ON
-		PORTB |= (1 << GREEN_LED);
-    }
-    else 
-    {
-		// GREEN LED OFF
-		PORTB &= ~(1 << GREEN_LED);
-    } 
-    if (blueValue != 0) 
-    { 
-		// BLUE LED ON
-		PORTB |= (1 << BLUE_LED);
-    }
-    else 
-    {
-		// BLUE LED OFF
-		PORTB &= ~(1 << BLUE_LED);
-    }
-  */
-    if (redValue == 0 && greenValue == 0 && blueValue == 0) { isOn = false ; }
-    else { isOn = true; }
+  analogWrite(RED_LED, redValue);
+  analogWrite(GREEN_LED, greenValue);
+  analogWrite(BLUE_LED, blueValue);
+ 
+  if (redValue == 0 && greenValue == 0 && blueValue == 0) { isOn = false ; }
+  else { isOn = true; }
 }
 
+// Receives and processes the IR signal
 void receive()
 {
-   if (receiver.decode(&results))
+  if (receiver.decode(&results))
   {
-    long int decCode = results.value;
-    //Serial.println(results.value,  HEX);
     switch (results.value)
     {
-      // ON/OFF Switch
-    case 737444966: 
-    case 3622325019:
-    isOn ? setRgb(0,0,0) : setRgb(1,1,1);
-    isOn = !isOn;
-    break;
-
-    case signal.ON:
-    setRgb(1,1,1);
-    // Red
-    case signal.RED:
-    setRgb(255,0,0);
-    //Serial.println("red\n");
-    break;
-
-    // Green
-    case signal.GREEN:
-    //Serial.println("green\n");
-    setRgb(0,255,0);
-    break;
-
-    // Blue
-    case signal.BLUE:
-    setRgb(0,0,255);
-    //Serial.println("blue\n");
-    break;
-
-    // Defaults to OFF
-    case signal.OFF:
-    setRgb(0,0,0);
-    break;
-
-    default:
-    break;
+      case signal.RED:
+      setRgb(255,0,0);
+      break;
+  
+      case signal.YELLOW:
+      setRgb(200,200,0);
+      break;
+  
+      case signal.GREEN:
+      setRgb(0,255,0);
+      break;
+  
+      case signal.CYAN:
+      setRgb(0,200,200);
+      break;
+  
+      case signal.BLUE:
+      setRgb(0,0,255);
+      break;
+      
+      case signal.MAGENTA:
+      setRgb(200,0,200);
+      break;
+  
+      case signal.WHITE:
+      setRgb(200,200,200);
+      break;
+  
+      default:
+      // Do nothing
+      break;
     }
     receiver.resume();
   }
 }
 
+// Called when IR signal is received
 void customISR()
 {
-  static unsigned long previousTime = 0;
+  static unsigned long previousTime {0};
   unsigned long currentTime = millis();
   isInterrupt = currentTime - previousTime > 100 ? true : false;
   previousTime = currentTime;
@@ -125,17 +117,13 @@ void customISR()
 
 void setup()
 {
-
+  // Set Pin 2 (PD2) as input
+  DDRD |= (1 << PD2);
+  
+  // Set LED Pins as output
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
   pinMode(BLUE_LED, OUTPUT);
-  pinMode(IR_RECEIVER, INPUT);
-
-	// Set PORTB as output
-	//DDRB = DDRB | 0xFF;
-
-	// Set PORTD as input
-	//DDRD = DDRD | 0x00; 
 
 	// Initialize variables
 	isOn = false;
@@ -150,8 +138,8 @@ void loop()
 {
   if (isInterrupt)
   {
+	// Receive and process the signal
     receive();
     isInterrupt = false;
   }
- //_delay_ms(100);
 }
